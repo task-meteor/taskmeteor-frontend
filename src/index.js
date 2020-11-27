f553213e52c2fd0c57fdc71b4775c9670efc113a
Alexander Heraimenka <a.heraimenka@gmail.com>
2020-11-27 15:41:17 -0800

    Add the root route for home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+
+
+import Home from "./pages/Home/Home";
+
 
 ReactDOM.render(
     <React.StrictMode>
-            <App />
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     </React.StrictMode>,
     document.getElementById("root")
 );
