f828a238bf5213188d0e86587f06e6432eeea588
Alexander Heraimenka <a.heraimenka@gmail.com>
2020-12-01 12:35:55 -0800

    Add an astronaut

--- src/common/Intro/Intro.js
+++ src/common/Intro/Intro.js
@@ -5,7 +5,7 @@ export default function Intro () {
         <div className="Intro">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-6">
+                    <div className="col-lg-6">
                         <h1>Boost Your Productivity</h1>
                         <p>
                             Task Meteor is a simple app that combines a timer and a task manager. It helps reduce distractions, limit interruptions and complete tasks more efficiently.
