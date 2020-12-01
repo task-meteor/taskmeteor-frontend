21dcc47bd18dd62b71c07b7bcea50abbc47b9dd4
Alexander Heraimenka <a.heraimenka@gmail.com>
2020-11-30 18:18:19 -0800

    Add intro block

---
+++ src/common/Intro/Intro.js
@@ -0,0 +1,21 @@
+import "./Intro.scss";
+
+export default function Intro () {
+    return (
+        <div className="Intro">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <h1>Boost Your Productivity</h1>
+                        <p>
+                            Task Meteor is a simple app that combines a timer and a task manager. It helps reduce distractions, limit interruptions and complete tasks more efficiently.
+                        </p>
+                        <p>
+                            <a className="button button-red" href="#">Button here</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import "./Home.scss";
 import Nav from "../../common/Nav/Nav";
+import Intro from "../../common/Intro/Intro";
 import Features from "../../common/Features/Features";
 
 function Home() {
     return (
         <div className="Home">
             <Nav />
+            <Intro />
             <Features />
         </div>
     );
